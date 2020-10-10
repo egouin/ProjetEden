e0600ed0df4f19f21001b79118e44a6eab95bda0
egouin <solariss73@hotmail.com>
2020-10-10 09:27:04 -0400

    First check-in

---
+++ ProjetEden/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjetEden.Global" Language="C#" %>
---
+++ ProjetEden/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetEden.Startup))]
+namespace ProjetEden
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
